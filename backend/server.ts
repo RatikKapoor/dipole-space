90e7951152ec4a3c189446d4a448b42d0a0c0126
Ratik Kapoor <ratikrk@gmail.com>
2021-09-18 10:50:51 -0600

    fix: Missing modules

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from "express";
+import { graphqlHTTP } from "express-graphql";
 
 const app = express();
 
 const PORT = 4000;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_req: Request, res: Response) => {
   res.send("Hello World");
 });
+
+app.use("/graphql", graphqlHTTP(options);
 
 app.listen(PORT, () => {
   console.log(`GraphQL server running on port ${PORT} `);
