8b3e36c0d4f7a53cc05545be01bf72b8a3a67ca4
Robert Brown <brown.rjb75@gmail.com>
2021-09-18 11:08:50 -0600

    feat: add base pages

---
+++ src/pages/Feed.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Feed: React.FC = () => {
+    return (
+        <>
+            <h1>Post</h1>
+            <h2>Gathering</h2>
+            <h2>Posts</h2>
+        </>
+    )
+}
---
+++ src/pages/Profile.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Profile: React.FC = () => {
+    return (
+        <>
+            <h1>You</h1>
+            <p>Your profile information</p>
+        </>
+    )
+}
