77438329bf24cffe62beced5547e701319152923
Ratik Kapoor <ratikrk@gmail.com>
2021-09-18 10:32:57 -0600

    feat: Add backend

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+const PORT = 4000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`GraphQL server running on port ${PORT} `);
+});
