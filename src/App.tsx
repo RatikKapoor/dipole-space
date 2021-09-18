841c230a7f1b986d4ca49cc60cb705e5f3e5063e
Ratik Kapoor <ratikrk@gmail.com>
2021-09-18 15:00:53 -0600

    feat: Add routing to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,29 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
 import "./styles/main.scss";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Chat } from "./pages/Chat";
+import { Profile } from "./pages/Profile";
+import { Feed } from "./pages/Feed";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/chat">
+          <Chat />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/feed">
+          <Feed />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
