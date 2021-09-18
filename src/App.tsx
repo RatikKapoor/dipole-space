1ab16d6fe5fbcc78dc89a312c64929c3e9ffecdd
Robert Brown <brown.rjb75@gmail.com>
2021-09-18 12:06:01 -0600

    feat: add common styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
-import "./App.scss";
+import "./styles/main.scss";
 
 const App: React.FC = () => {
   return (
