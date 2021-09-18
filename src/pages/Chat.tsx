adaecbc72ca773955a90c35c4028072e90353837
Robert Brown <brown.rjb75@gmail.com>
2021-09-18 16:40:39 -0600

    feat: add listing for chat component

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,15 +1,19 @@
-import { Button, TextField } from "@mui/material";
-import SendIcon from "@mui/icons-material/Send";
+import { Button, List, ListItem, ListItemText, TextField } from "@mui/material";
 import React from "react";
 
 export const Chat: React.FC = () => {
   return (
     <>
-      <div>
-        <TextField variant="standard" label="Message" />
-        <Button variant="contained" endIcon={<SendIcon />}>
-          Send
-        </Button>
+      <div className="page">
+        <h2 className="section-header">Messages</h2>
+        <List>
+            <ListItem>
+                <ListItemText 
+                    primary={"Neo"}
+                    secondary={"Yeah, we can meet up tomorrow!"}
+                />
+            </ListItem>
+        </List>
       </div>
     </>
   );
