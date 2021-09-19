b57a80639d8c570aa436629d1841c76067fa44d5
Ratik Kapoor <ratikrk@gmail.com>
2021-09-19 09:49:29 -0600

    feat: Matching

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import "../styles/profile.scss";
 import { NavigationBar } from "../components/common/NavigationBar";
+import { Matching } from './Matching'
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/user/userSlice";
 
 export const Profile: React.FC = () => {
+    const { user } = useSelector(selectUser);
+
     return (
         <>
-            <div>
-                User Profile
-            </div>
+            {user &&
+                <Matching
+                    id={user._id}
+                    age={user.birthDate}
+                    firstName={user.firstName}
+                    bio={user.bio ?? ""}
+                    gender={user.gender}
+                    hobbies={user.hobbies}
+                    isOwnProfile={true}
+                />
+            }
             <NavigationBar />
         </>
     );
