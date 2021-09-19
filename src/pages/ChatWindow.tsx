de73735cc4a0920069e383a255fabac158a5e674
Robert Brown <brown.rjb75@gmail.com>
2021-09-18 19:33:38 -0600

    fix: fix import issue:

--- src/pages/ChatWindow.tsx
+++ src/pages/ChatWindow.tsx
@@ -1,5 +1,4 @@
-import { Button, List, ListItem, ListItemText, TextField } from "@mui/material";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import React from "react";
 import { ArrowBack, ChevronRight } from "@mui/icons-material";
 
@@ -8,29 +7,23 @@ export const ChatWindow: React.FC = () => {
     <>
       <div className="page message-window">
         <div className="message-header">
-            <div className="back-button">
-                <Link to="/chat">
-                    <ArrowBack />
-                </Link>
-            </div>
-            <h2>Carter</h2>
-            <a className="profile-link">
-                <span>View Profile</span>
-                <ChevronRight />
-            </a>
+          <div className="back-button">
+            <Link to="/chat">
+              <ArrowBack />
+            </Link>
+          </div>
+          <h2>Carter</h2>
+          <a className="profile-link">
+            <span>View Profile</span>
+            <ChevronRight />
+          </a>
         </div>
         <div className="message-compose">
-            <div className="compose-container">
-                <div className="take-picture">
-
-                </div>
-                <div>
-
-                </div>
-                <div className="upload-image">
-
-                </div>
-            </div>
+          <div className="compose-container">
+            <div className="take-picture"></div>
+            <div></div>
+            <div className="upload-image"></div>
+          </div>
         </div>
       </div>
     </>
