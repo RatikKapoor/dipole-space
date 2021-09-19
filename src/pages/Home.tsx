1f7cbbb0616a861903b3639946681ead777dd640
Robert Brown <brown.rjb75@gmail.com>
2021-09-19 09:52:57 -0600

    feat: update style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import SkiImage from "../styles/images/activity-ski.png"
 import PaintImage from "../styles/images/activity-paint.png"
 import FemaleAvatar from "../styles/images/female-avatar.png"
 import MaleAvatar from "../styles/images/male-avatar.png"
-import { ArrowForwardRounded, FavoriteBorderRounded } from "@mui/icons-material";
+import { ArrowForwardRounded, ChevronRight, FavoriteBorderRounded } from "@mui/icons-material";
 import HomeWavesTop from '../styles/vectors/home-waves-top.svg'
 
 export const Home: React.FC = () => {
@@ -16,37 +16,46 @@ export const Home: React.FC = () => {
                     <img src={HomeWavesTop} className="home-top-waves" />
                     <div className="activity-post">
                         <div className="post-header">
-                            <img src={MaleAvatar} />
+                            <img className="avatar" src={MaleAvatar} />
                             <h2>Carter</h2>
                             <div className="activity-metric">
                                 <Icon size={"30px"} icon={"ski-icon"} />
-                                <p>23 Km/Week</p>
+                                <span>23 Km/Week</span>
                             </div>
                         </div>
                         <img src={SkiImage} />
                         <div className="activity-actions">
                             <FavoriteBorderRounded />
                             <ArrowForwardRounded />
-                            <p className="activity-timeline"> 1 hour ago</p>
+                            <span className="activity-timeline"> 1 hour ago</span>
                         </div>
                         <div className="activity-description">
                             <p>Ski trip to Banff National Park! Going again this weekend. Join us! </p>
                         </div>
                     </div>
+                    <div className="activity-link">
+                        <div className="activity-button">
+                            <div className="button-text">
+                                <span>Snoeshowing with Alexis</span>
+                                <span>December 21st, 2021</span>
+                            </div>
+                            <ChevronRight />
+                        </div>
+                    </div>
                     <div className="activity-post">
                         <div className="post-header">
-                            <img src={FemaleAvatar} />
+                            <img className="avatar" src={FemaleAvatar} />
                             <h2>Don</h2>
                             <div className="activity-metric">
-                                <Icon size={"30px"} icon={"ski-icon"} />
-                                <p>25 paintings <br/> completed</p>
+                                <Icon size={"30px"} icon={"paint-icon"} />
+                                <span>25 paintings <br/> completed</span>
                             </div>
                         </div>
                         <img src={PaintImage} />
                         <div className="activity-actions">
                             <FavoriteBorderRounded />
                             <ArrowForwardRounded />
-                            <p className="activity-timeline"> 1 hour ago</p>
+                            <span className="activity-timeline"> 1 hour ago</span>
                         </div>
                         <div className="activity-description">
                             <p>Join us for a creative weekend out in Victoria to channel your next Bob Ross!</p>
